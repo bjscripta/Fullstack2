1720de60931b4cdb0ae601702662cfd4627d712a
Geneesiis <ge.floresm@duocuc.cl>
2025-05-13 21:46:45 -0400

    Add files via upload
    Logistica + base de datos

---
+++ perfulandia/perfulandia/src/main/java/com/perfulandia/repository/EnvioRepository.java
@@ -0,0 +1,7 @@
+package com.perfulandia.repository;
+
+import com.perfulandia.model.Envio;
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+public interface EnvioRepository extends JpaRepository<Envio, Integer>{  
+}
