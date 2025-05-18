480ec368e87705c8cb317df40dd6814417cd0a98
Geneesiis <ge.floresm@duocuc.cl>
2025-05-17 20:18:36 -0400

    Add files via upload
    logística

---
+++ Logistica/perfulandia Usuarios/perfulandia/src/main/java/com/perfulandia/perfulandia/repository/ProveedorRepository.java
@@ -0,0 +1,7 @@
+package com.perfulandia.perfulandia.repository;
+
+import com.perfulandia.perfulandia.model.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveedorRepository extends JpaRepository<Proveedor, Integer>{  
+} 
---
+++ Logistica/perfulandia Usuarios/perfulandia/src/main/java/com/perfulandia/perfulandia/repository/RutaEntregaRepository.java
@@ -0,0 +1,7 @@
+package com.perfulandia.perfulandia.repository;
+
+import com.perfulandia.perfulandia.model.RutaEntrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RutaEntregaRepository extends JpaRepository<RutaEntrega, Integer>{
+}
