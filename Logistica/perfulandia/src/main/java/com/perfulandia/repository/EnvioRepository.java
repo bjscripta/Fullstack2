984f7f355879a5ee5eac764342bf462509c1ccca
Geneesiis <ge.floresm@duocuc.cl>
2025-05-17 20:17:06 -0400

    Delete Logistica/perfulandia directory

--- Logistica/perfulandia/src/main/java/com/perfulandia/repository/EnvioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.repository;
-
-import com.perfulandia.model.Envio;
-import org.springframework.data.jpa.repository.JpaRepository; 
-
-public interface EnvioRepository extends JpaRepository<Envio, Integer>{  
-}
--- Logistica/perfulandia/src/main/java/com/perfulandia/repository/RutaEntregaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.perfulandia.repository;
-
-import com.perfulandia.model.RutaEntrega;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RutaEntregaRepository extends JpaRepository<RutaEntrega, Integer>{
-}
