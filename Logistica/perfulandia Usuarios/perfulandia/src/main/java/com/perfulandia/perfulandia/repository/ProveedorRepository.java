2d6161a7243416200d20928dac117d13800d6c8d
Geneesiis <ge.floresm@duocuc.cl>
2025-05-17 23:47:00 -0400

    Delete Logistica/perfulandia Usuarios/perfulandia directory

--- Logistica/perfulandia Usuarios/perfulandia/src/main/java/com/perfulandia/perfulandia/repository/ProveedorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.perfulandia.repository;
-
-import com.perfulandia.perfulandia.model.Proveedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProveedorRepository extends JpaRepository<Proveedor, Integer>{  
-} 
--- Logistica/perfulandia Usuarios/perfulandia/src/main/java/com/perfulandia/perfulandia/repository/RutaEntregaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.perfulandia.repository;
-
-import com.perfulandia.perfulandia.model.RutaEntrega;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RutaEntregaRepository extends JpaRepository<RutaEntrega, Integer>{
-}
